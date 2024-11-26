b2b856e65ea7a0b7a48de95a5680724b8500e20c
rsancho64 <rsancho@iesrioarba.es>
2024-11-27 00:29:31 +0100

    .

---
+++ README.md
@@ -0,0 +1,1 @@
+https://enriquelazcorreta.gitbooks.io/tkinter/content/introduccion-a-la-programacion-visual/la-programacion-orientada-a-eventos.html
---
+++ eventos.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+# intro POE w py : https://enriquelazcorreta.gitbooks.io/tkinter/content/
+# un ejemplo:
+
+from tkinter import *
+
+def key(event):
+    print("tecla pulsada:", repr(event.char))
+
+def callback(event):
+    frame.focus_set()
+    print("click en", event.x, event.y)
+
+if __name__ == "__main__":
+
+    root = Tk()
+
+    frame = Frame(root, width=200, height=200)
+    frame.bind("<Key>", key)
+    frame.bind("<Button-1>", callback)
+    frame.pack()
+
+    root.mainloop()
+
