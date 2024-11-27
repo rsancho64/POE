f5c9d8c8ac90fab420392e5a6ee166e5a2ec7b98
rsancho64 <rsancho@iesrioarba.es>
2024-11-28 00:31:44 +0100

    .

---
+++ girarCoinPic.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# origen: https://recursospython.com/guias-y-manuales/posicionar-elementos-en-tkinter/
+
+# import tkinter as tk
+# from tkinter import ttk
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter import * 
+
+class Application(ttk.Frame):
+    
+    def __init__(self, main_window):
+        super().__init__(main_window)
+        main_window.title("Posicionar elementos en Tcl/Tk")
+        
+        main_window.configure(width=300, height=300)
+        # Ignorar esto por el momento.
+        self.place(relwidth=1, relheight=1)
+
+        # self.entry = ttk.Entry(self)
+        # self.entry.pack()
+
+        self.button = tk.ttk.Button(self, text="girar")
+        # self.button.place(x=60, y=40)
+        # self.button.place(relx= relwidth=0.5, relheight=0.5)
+        # self.button.place(relx=0.5, rely=0.5, relwidth=0.5, relheight=0.5)
+        self.button.pack(expand=True, fill=tk.BOTH, padx=10, pady=10)
+
+        self.button.pack()
+        # self.pack()
+                        
+    def setButtonImage(self, img):
+        self.button.destroy()
+        self.button = ttk.Button(self, image = img) #.pack(side = TOP)
+                
+if __name__ == "__main__":
+
+    main_window = Tk()
+    app = Application(main_window)
+
+    #  photoimage object to use image 
+    photoCara = PhotoImage(file = r"./duro.cara.jpeg")
+    # photoCruz = PhotoImage(file = r"./duro.cruz.jpeg")
+
+    # app.setButtonImage(photoCara)
+    # app.setButtonImage(ph0toCruz)
+    
+    app.mainloop()
+
